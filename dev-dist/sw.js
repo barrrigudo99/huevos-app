c6fec7772aa79082a961e37d4f1f6b398826f200
barrrigudo99 <carlosbarrientoslopez@gmail.com>
2026-09-07 13:43:30 +0200

    v2 subida 070926

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.aelubdlp0eg"
+    "revision": "0.djacpt9q648"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
